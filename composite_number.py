e26155b3b5c090f8fc8ac370233f9a790a788c1d
Emil <karlemil.lindberg@gmail.com>
2012-11-15 12:08:13 +0100

    Bilder and woark!

---
+++ composite_number.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  composite_number.py
+#  
+#  Copyright 2012 Emil <emil@emil-luftbunt>
+#  
+
+from Tkinter import Canvas
+
+class CompositeNumber(Canvas):
+	def __init__(self, no_numbers):
--- lotto.py
+++ lotto.py
@@ -6,7 +6,7 @@
 #  Copyright 2012 Emil <emil@emil-luftbunt>
 #  
 
-from Tkinter import *
+import Tkinter as tk
 from time import sleep, clock
 from animated_number import AnimatedNumber
 from number_canvas import NumberCanvas
@@ -19,7 +19,7 @@ def roll_text():
     return 'Nytt nummer!'
 
 def init_menu(root):
-    menu = Menu(root)
+    menu = tk.Menu(root)
     root.config(menu=menu)
     
     menu.add_command(label="Nytt lotteri", command=new_raffle_callback)
@@ -39,39 +39,49 @@ def configure_callback(event):
     
 def init_images():
     number_images_paths = {0:'bilder/zero.gif', 1:'bilder/one.gif', 2:'bilder/two.gif'}
-    images.append(PhotoImage(file=number_images_paths[0]))
-    images.append(PhotoImage(file=number_images_paths[1]))
-    images.append(PhotoImage(file=number_images_paths[2]))
+    images.append(tk.PhotoImage(file=number_images_paths[0]))
+    images.append(tk.PhotoImage(file=number_images_paths[1]))
+    images.append(tk.PhotoImage(file=number_images_paths[2]))
 
 def main():
     global run
     i = 0
     delta = clock()
     
-    root = Tk()
+    root = tk.Tk()
     init_images()
     init_menu(root)
     root.title("Lotto!")
     
     root.protocol('WM_DELETE_WINDOW', destroy_callback)
     root.config(width=500, height=500)
+
+
     
-    left_bar = Frame(root)
-    roll_button = Button(left_bar, text=roll_text(), command=roll_callback)
-    roll_button.pack(side=TOP, padx=2, pady=10)
-    left_bar.pack(side=LEFT, fill=Y)
+    # left_bar = Frame(root, bg='')
+    # roll_button = Button(left_bar, text=roll_text(), command=roll_callback)
+    # roll_button.pack(side=TOP, padx=2, pady=10)
+    # left_bar.pack(side=LEFT, fill=Y)
     
-    main_canvas = NumberCanvas(root)
+    main_canvas = NumberCanvas(root, bd=0, highlightthickness=0, relief='ridge')
     main_canvas.bind('<Configure>', configure_callback)
-    
+
+    background_photo_image = tk.PhotoImage(file='bilder/BG.gif')
+    side_thing = tk.PhotoImage(file='bilder/sidsak.gif')
+    background_image = main_canvas.create_image(0, 0, image=background_photo_image, anchor=tk.NW)
+    side_image = main_canvas.create_image(0, 0, image=side_thing, anchor=tk.NW)
+
     main_canvas.add_number(AnimatedNumber(main_canvas, 100, 100, 0.5, 0, images, 0, 2))
+
+    _button = tk.Button(main_canvas, text=roll_text(), command=roll_callback, image=images[0])
+    _button.place(anchor=tk.S, relx=0.5, rely=1)
     
-    main_canvas.pack(side=LEFT, fill=BOTH, expand=1)
+    main_canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
     
     while run:
         delta = clock() - delta
         main_canvas.get_numbers()[0].update(delta)
-        
+
         root.update()
         sleep(0.020)
         
